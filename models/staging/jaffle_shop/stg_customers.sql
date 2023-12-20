32d5557385b751cf7f3c8d17c3481aa47366d762
Michelle Baird <michelle.baird@fishtownanalytics.com>
2023-12-20 15:51:52 +0000

    forgot to save a file

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -14,7 +14,7 @@ renamed as (
         id as customer_id,
 
         ---------- properties
-        first_name || ' ' || last_name as customer_name
+        name as customer_name
 
     from source
 
